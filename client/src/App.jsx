838e5769e2e95ad02d5cbdcfdf3f6cb184d51919
Akieboye Godgift <akiboyegodgift@gmail.com>
2024-05-16 15:42:24 +0100

    3rd commit

---
+++ client/src/App.jsx
@@ -0,0 +1,19 @@
+import { Footer, Loader, Navbar, Services, Transactions, Welcome } from "./components"
+
+const App = () => {
+
+  return (
+    <div className="min-h-screen">
+      <div class="gradient-bg-welcome">
+        <Navbar />
+        <Welcome />
+      </div>
+    
+      <Services />
+      <Transactions />
+      <Footer />
+    </div>
+  )
+}
+
+export default App
